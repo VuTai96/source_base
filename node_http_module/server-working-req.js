4fe444de8e0f21a8d172febace7359325f65ae5b
VuTai96 <vutai13196@gmail.com>
2025-10-23 10:02:22 +0700

    feat: http - working with URLs - queryString

--- node_http_module/server-working-req.js
+++ node_http_module/server-working-req.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const querystring = require('querystring');
 
 const PORT = 3000;
 
@@ -12,10 +13,23 @@ const server = http.createServer((req, res) => {
     const pathname = parsedURL.pathname; // The path without query string
     const query = parsedURL.query; // The query string as an object
 
+    const baseURL = 'http://' + req.headers.host + '/';
+    const parsedFullURL = new url.URL(req.url, baseURL);
+    const params = parsedFullURL.searchParams;
+    console.log(parsedFullURL);
+
+    const queryObj = {
+        name: 'Tai Vu',
+        age: 29,
+        interests: ['Programming', 'music'],
+    };
+    const queryStr = querystring.stringify(queryObj);
+
     res.writeHead(200, {
         'content-type': 'text/plain',
     });
     res.end(`
+        -----
         User-Agent: ${userAgent}
         Accept-Language: ${acceptLanguage}
         You made a ${req.method} request to ${req.url}
@@ -23,6 +37,11 @@ const server = http.createServer((req, res) => {
         fullURL: ${req.url}
         Pathname: ${pathname}
         query: ${JSON.stringify(query)}
+        -----
+        path: ${parsedFullURL.pathname}
+        params: ${params}
+        queryStr: ${queryStr}
+
         `);
 });
 
