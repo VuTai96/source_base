29453f85bf99d35e6a854773c6c760e5d47ea47e
VuTai96 <vutai13196@gmail.com>
2025-10-23 09:41:20 +0700

    feat: http - working with URLs

--- node_http_module/server-working-req.js
+++ node_http_module/server-working-req.js
@@ -1,17 +1,29 @@
 const http = require('http');
+const url = require('url');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log('Request Headers: ', req.headers);
-
     const userAgent = req.headers['user-agent'];
     const acceptLanguage = req.headers['accept-language'];
+    //parse the URL
+    const parsedURL = url.parse(req.url, true);
+    // get different parts of the URL
+    const pathname = parsedURL.pathname; // The path without query string
+    const query = parsedURL.query; // The query string as an object
 
     res.writeHead(200, {
         'content-type': 'text/plain',
     });
-    res.end(`User-Agent: ${userAgent}\nAccept-Language: ${acceptLanguage}`);
+    res.end(`
+        User-Agent: ${userAgent}
+        Accept-Language: ${acceptLanguage}
+        You made a ${req.method} request to ${req.url}
+        -----
+        fullURL: ${req.url}
+        Pathname: ${pathname}
+        query: ${JSON.stringify(query)}
+        `);
 });
 
 server.listen(PORT, () => {
