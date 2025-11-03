51dd3afbb15a711d4ad02023e28b9ef2802bcf4f
VuTai96 <vutai13196@gmail.com>
2025-11-03 11:28:49 +0700

    chore: http.clientRequest - add POST method

--- node_http_module/http.client/connect.client.js
+++ node_http_module/http.client/connect.client.js
@@ -15,8 +15,22 @@ req.on('connect', (res, socket, head) => {
     console.log('[CLIENT] Turnnel established through proxy');
 
     // make HTTP request over the established tunnel
-    socket.write('GET / HTTP/1.1\r\n' + 'Host: 127.0.0.1:3000\r\n' + 'Connection: close\r\n' + '\r\n');
+    const getRequest = 'GET / HTTP/1.1\r\n' + 'Host: 127.0.0.1:3000\r\n' + 'Connection: keep-alive\r\n' + '\r\n';
+    socket.write(getRequest);
 
+    // send POST after GET 1000ms later
+    setTimeout(() => {
+        const postBody = JSON.stringify({ msg: 'Hello from client!' });
+        const postRequest =
+            'POST / HTTP/1.1\r\n' +
+            'Host: 127.0.0.1:3000\r\n' +
+            'Content-Type: application/json\r\n' +
+            `Content-Length: ${Buffer.byteLength(postBody)}\r\n` +
+            'Connection: close\r\n' +
+            '\r\n' +
+            postBody;
+        socket.write(postRequest);
+    }, 1000);
     // receive response from target server
     socket.on('data', (chunk) => {
         console.log(`[CLIENT] Received data:\n${chunk.toString()}`);
--- node_http_module/http.client/connect.server.js
+++ node_http_module/http.client/connect.server.js
@@ -2,8 +2,26 @@ const http = require('http');
 
 server = http.createServer((req, res) => {
     console.log(`[SERVER] Received request: ${req.method} ${req.url}`);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from the real server!\n');
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        if (req.method === 'POST') {
+            console.log(`[SERVER] POST - Request body: ${body}`);
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.end(
+                JSON.stringify({
+                    message: 'POST request received',
+                    receivedBody: body,
+                }),
+            );
+        } else {
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.end('Hello from server via GET request!\n');
+        }
+    });
 });
 
 server.listen(3000, () => {
