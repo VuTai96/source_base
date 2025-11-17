ae2dca78af0d7ac9d8e6b7a4167e5ac524cca8a1
VuTai96 <vutai13196@gmail.com>
2025-11-17 14:31:18 +0700

    express, getting-start - static file

--- express/getting-start/app.js
+++ express/getting-start/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const POST = 3000;
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public2')));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
