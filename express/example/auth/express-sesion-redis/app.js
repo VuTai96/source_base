89513a1898bb66030815be5a66d587caa46227d8
VuTai96 <vutai13196@gmail.com>
2025-11-17 17:19:11 +0700

    express-session, redis - add env

--- express/example/auth/express-sesion-redis/app.js
+++ express/example/auth/express-sesion-redis/app.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const redis = require('redis');
+require('dotenv').config();
 
 // Tạo Redis client
 const redisClient = redis.createClient({
     legacyMode: true,
     socket: {
-        host: 'redis',
-        port: 6379,
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
     },
 });
 redisClient.connect().catch(console.error);
