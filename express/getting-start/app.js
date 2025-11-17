cf2c186e5a856e20cab9130f2679b218f6dbe20f
VuTai96 <vutai13196@gmail.com>
2025-11-17 14:19:24 +0700

    express, getting-start - method

--- express/getting-start/app.js
+++ express/getting-start/app.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.post('/', (req, res) => {
+    res.send('Got a POST request');
+});
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT at /user');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE rerquest at /user');
+});
+
 app.listen(POST, () => {
     console.log(`[Server] is listening on http://localhost:${POST}`);
 });
